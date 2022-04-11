34acaf24a7ade07dc2a396fdfa7d2350431c5dcb
drac94 <info@luisguerrero.me>
2022-04-11 12:42:15 -0500

    fix test

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/client';
 import { render, screen } from '@testing-library/react';
+
+import { client } from '../../apolloConfig';
 
 import Home from './Home';
 
 test('renders learn react link', () => {
-  render(<Home />);
+  render(
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
+  );
   const headerElement = screen.getByText(/Home/i);
   expect(headerElement).toBeInTheDocument();
 });
